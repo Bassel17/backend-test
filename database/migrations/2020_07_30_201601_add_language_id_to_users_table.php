477ae20db9274225dff2009ca040b386519e9510
Bassel <basselkanso82@gmail.com>
2020-07-30 23:39:49 +0300

    fix: add foreign key restraints to the tables

--- database/migrations/2020_07_30_201601_add_language_id_to_users_table.php
+++ database/migrations/2020_07_30_201601_add_language_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddLanguageIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('language_id');
+            $table->foreignId('language_id')->references('id')->on('languages');
         });
     }
 
