72069cfd35b34cb71be050fcaa5452e7c790dc0b
Bassel <basselkanso82@gmail.com>
2020-08-01 00:31:55 +0300

    fix: make description into a text type

--- database/migrations/2020_07_30_160506_create_posts_table.php
+++ database/migrations/2020_07_30_160506_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users');
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
